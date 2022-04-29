7ffcf3bfc48732ace89d470af503dd37554beb9a
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-04-29 16:52:13 +0000

    done

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Created by: Jaejun Lee
+// Created on: April 2022
+// This file contains the JS functions for index.html
+
+"use strict";
+
+/**
+ * This function determines if an integer is negative or positive.
+ */
+function myButtonClicked() {
+  const userInteger = parseInt(document.getElementById("integer").value);
+  if (userInteger > -1) {
+    document.getElementById("answer").innerHTML =
+      "The integer is positive.";
+  } else {
+    document.getElementById("answer").innerHTML =
+      "The integer is negative.";
+  }
+}
